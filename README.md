2f748674eab1fb40d22774819742cb18910a6a9a
andreacorrea10 <99226514+andreacorrea10@users.noreply.github.com>
2022-03-26 18:25:23 -0300

    Projeto API

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# API-Cadastro-Login
- 
+<h1>PROJETO API CADASTRO LOGIN</h1>
+<p>Projeto criado em <a href="https://nodejs.org/en/">NodeJS</a></p>
+
+<h2>Frameworks utilizadas:</h2>
+ <a href="https://www.npmjs.com/package/bcrypt">Bcrypt</a>, <a href="https://www.npmjs.com/package/dotenv">Dotenv</a>, 
+<a href="https://www.npmjs.com/package/express">Express</a>, <a href="https://www.npmjs.com/package/jsonwebtoken">JWT</a>, <a href="https://www.npmjs.com/package/mongoose">Mongoose</a> e <a href="https://www.npmjs.com/package/nodemon">Nodemon</a>(Dev Dependecies).
+
+Utilizado <a href="https://www.mongodb.com/atlas">MongoDB Atlas</a>.
+
+Sistema de cadastro de usuários e login pensado para empresas do segmento bancário.
+
+Rotas GET e POST;
+Rota pública, Registro de usuários, Reforço de senhas com Bcrypt gerando Hash e incluindo algoritimos;
+Sistema com diversas validações para proteger os dados enviados;
+
+Solicitação de dados como CPF e RG para cadastro, sendo os dados protegidos no Banco de dados.
+Rota de login com Token para o usuário e validação do mesmo através do Token;
+
+Rota privada e autenticação completa com <a href="https://www.npmjs.com/package/jsonwebtoken">JWT</a>.
+
+API testada no <a href="https://www.postman.com/">Postman</a>.
---
+++ app.js
@@ -0,0 +1,185 @@
+//Imports
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+
+//Config Json response
+app.use(express.json())
+
+//Models
+const User = require ('./models/User')
+
+//Routes - open, public
+
+app.get('/', (req,res) => {
+    res.status(200).json({ msg: 'Bem-vindo a API' })
+})
+
+//Private routes
+app.get('/user/:id', checkToken, async (req, res) => {
+    const id = req.params.id
+
+//check if user exists
+const user = await User.findById(id, '-password -cpf -rg')
+
+if (!user) {
+    return res.status(404).json({ msg: 'Usuário não encontrado!'})
+}
+
+res.status(200).json({ user })
+
+})
+
+function checkToken(req, res, next) {
+
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) {
+        return res.status(401).json({ msg: 'Acesso negado!' })
+    }
+
+    try{
+
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+    } catch(error) {
+        res.status(400).json({ msg: 'Token inválido!' })
+    }
+}
+
+//Register User
+app.post('/auth/register', async (req, res) => {
+    const {name, email, password, confirmpassword, cpf, rg} = req.body
+
+//Validations
+    if(!name) {
+        return res.status(422).json({ msg: 'O campo NOME é obrigatório!' })
+    }
+
+    if(!email) {
+        return res.status(422).json({ msg: 'O campo EMAIL é obrigatório!' })
+    }
+
+    if(!password) {
+        return res.status(422).json({ msg: 'O campo SENHA é obrigatório!' })
+    }
+
+    if(password !== confirmpassword) {
+        return res.status(422).json({ msg: 'As senhas não são idênticas!' })
+    }
+
+    if(!cpf) {
+        return res.status(422).json({ msg: 'O campo CPF é obrigatório!' })
+    }
+
+    if(!rg) {
+        return res.status(422).json({ msg: 'O campo RG é obrigatório!' })
+    }
+
+//Check if user exists
+const userExists = await User.findOne({ email: email })
+    if(userExists) {
+        return res.status(422).json({ msg: 'Este EMAIL ja está cadastrado!' })  
+}
+
+const cpfExists = await User.findOne({ cpf: cpf })
+    if(cpfExists) {
+        return res.status(422).json({ msg: 'Este CPF ja está cadastrado!' })  
+}
+
+const rgExists = await User.findOne({ rg: rg })
+    if(rgExists) {
+        return res.status(422).json({ msg: 'Este RG ja está cadastrado!' })  
+}
+
+//Create password
+const salt = await bcrypt.genSalt(12)
+const passwordHash = await bcrypt.hash(password, salt)
+
+//Create User
+const user = new User ({
+   name,
+   email,
+   password: passwordHash,
+   cpf,
+   rg,
+})
+
+try {
+    await user.save()
+
+    res.status(201).json({msg: 'Usuário cadastrado com sucesso!'})
+
+} catch(error) {
+    console.log(error)
+
+    res.status(500).json({ msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!' })
+}
+
+})
+
+//Login User
+app.post('/auth/login', async(req, res) => {
+
+    const {email, password} =req.body
+
+//Validation
+if(!email) {
+    return res.status(422).json({ msg: 'O campo EMAIL é obrigatório!' })
+}
+
+if(!password) {
+    return res.status(422).json({ msg: 'O campo SENHA é obrigatório!' })
+}
+
+//Check if user exists
+const user = await User.findOne({ email: email })
+    if(!user) {
+        return res.status(404).json({ msg: 'Usuário não encontrado!' }) 
+    }
+
+//Check if password match
+const checkPassword = await bcrypt.compare(password, user.password)
+
+if(!checkPassword) {
+    return res.status(422).json({ msg: 'Senha inválida!' })
+}
+
+try {
+
+    const secret = process.env.SECRET
+
+    const token = jwt.sign({
+        id: user._id,
+    }, secret,
+    )
+    res.status(200).json({ msg: 'Autenticação realizada com sucesso!', token })
+
+} catch(err) {
+    console.log(error)
+
+    res.status(500).json({ msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!' })
+}
+
+})
+
+
+//Credentials
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose.connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.69msx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    ).then(() => {
+    app.listen(3000)
+    console.log('Connected to Database')
+}).catch((err) => console.log(err))
+
